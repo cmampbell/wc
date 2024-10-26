1bd78757b5a47d9a48778a9de62a402c7fd1e559
Matt Campbell <cmampbell@gmail.com>
2024-10-26 16:01:40 +0000

    wrote notes for process

--- wc/Program.cs
+++ wc/Program.cs
@@ -7,14 +7,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
         //File.Open(".\test.txt");
-
+        //need to get argument from command line
+        // need to recognize -c as count the bytes
+        // need to open the file
+        // need to count the bytes in a fule
+        // need to return the byte count to the console
         if (args.Length > 0)
         {
-            foreach (var arg in args)
-            {
-                Console.WriteLine($"Argument={arg}");
+            if(args[0] == "-c"){
+                Console.WriteLine("In -c");
             }
         }
         else
