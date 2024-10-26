56f119442da356dd38b9ce7aa449b783756b37b4
Matt Campbell <cmampbell@gmail.com>
2024-10-26 14:21:13 +0000

    created project

---
+++ wc/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace wc;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        //File.Open(".\test.txt");
+
+        if (args.Length > 0)
+        {
+            foreach (var arg in args)
+            {
+                Console.WriteLine($"Argument={arg}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("No arguments");
+        }
+    }
+}
+
