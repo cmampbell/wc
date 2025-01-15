0bfc2151ac9ccc294376e0d7d9b5f2f007fe4b36
Matt Campbell <cmampbell@gmail.com>
2025-01-15 02:49:34 +0000

    cleaned up CountWords function, close

--- wc/Program.cs
+++ wc/Program.cs
@@ -55,50 +55,24 @@ class Program
 
     static void CountWords(FileInfo file)
     {
-        // implement word count function
-
-        string[] text = File.ReadLines(file.FullName).ToArray();//.Length
-        for (int i = 0; i < 50; i++)
+        string text = File.ReadAllText(file.FullName);
+        int count = 0;
+        bool inWord = false;
+        for (int i = 0; i < text.Length; i++)
         {
-            int count = 0;
-            bool inWord = false;
-            string line = text[i];
-            for (int j = 0; j < line.Length; j++)
-            {
-                char c = line[j];
+            char c = text[i];
 
-                if ((!char.IsLetter(c)) && inWord)
+             if (char.IsWhiteSpace(c))// && inWord)
                 {
                     inWord = false;
                 }
-                else if (char.IsLetter(c) && !inWord)
+                else if (char.IsLetterOrDigit(c) && !inWord)
                 {
                     inWord = true;
                     count++;
                 }
-            }
-            Console.WriteLine(line);
-            Console.WriteLine(count);
         }
 
-        // string text = File.ReadAllText(file.FullName);
-        // int count = 0;
-        // bool inWord = false;
-        // for (int i = 0; i < text.Length; i++)
-        // {
-        //     char c = text[i];
-
-        //     if ((!char.IsLetter(c)) && inWord)
-        //     {
-        //         inWord = false;
-        //     }
-        //     else if (char.IsLetter(c) && !inWord)
-        //     {
-        //         inWord = true;
-        //         count++;
-        //     }
-        // }
-
-        //Console.WriteLine($"{count} {file}");
+        Console.WriteLine($"{count} {file}");
     }
 }
